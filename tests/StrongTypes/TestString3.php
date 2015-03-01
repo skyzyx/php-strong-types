31699b05a310d52c9e5bec89121babc0035101c5
Ryan Parman <ryan@ryanparman.com>
2015-02-28 22:06:47 -0800

    Updated the copyright year for 2015.

--- tests/StrongTypes/TestString3.php
+++ tests/StrongTypes/TestString3.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Copyright (c) 2014 Ryan Parman.
+ * Copyright (c) 2014-2015 Ryan Parman.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
